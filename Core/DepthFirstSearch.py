0af3619f2becac1630afcc075c6bc12000f2733b
louay-hesham <louay.hesham@gmail.com>
2017-07-26 23:26:38 +0200

    added search technique class

---
+++ Core/DepthFirstSearch.py
@@ -0,0 +1,13 @@
+from Core.Search import Search
+
+class DepthFirstSearch(Search):
+
+    def __init__(self, start_node):
+        return super().__init__(start_node)
+        
+
+    def get_solution(self, maze):
+
+        
+
+
