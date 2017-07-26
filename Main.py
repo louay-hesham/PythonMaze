57f502e76667435431787d24135f2ad79d6a0dca
louay-hesham <louay.hesham@gmail.com>
2017-07-26 23:28:17 +0200

    changes for testing

--- Main.py
+++ Main.py
@@ -1,5 +1,11 @@
 from Core import *
-from GUI import MainGUI 
+from GUI import *
 
 x = MainGUI()
 x.on_execute()
+maze = x.maze
+n = Node(0,3,3,maze)
+
+print ("printing")
+print (n.get_children_nodes())
+print ("done")
