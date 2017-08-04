ae52c32fc6ab6dfe327840362c5ce15d4dc0fab3
louay-hesham <louay.hesham@gmail.com>
2017-08-04 14:44:44 +0200

    Modified search to make it run step by step

--- Core/Search.py
+++ Core/Search.py
@@ -9,6 +9,8 @@ class Search(object):
         self.maze = maze
         self.start_node = start_node
         self.ds = []
+        self.prev_node = None
+        self.mode = 0
         self.visited = [None] * maze.height
         for i in range(0,maze.height):
             self.visited[i] = [None] * maze.length
@@ -25,10 +27,51 @@ class Search(object):
             self.visited[i] = [None] * self.maze.length
             for j in range(0, self.maze.length):
                 self.visited[i][j] = [False] * self.maze.width
+        
+        if self.mode == 2:
+            self.__init_BFS()
+
+    def __init_BFS(self):
+        self.ds.append(self.start_node)
+        self.visited[self.start_node.i][self.start_node.j][self.start_node.k] = True
+
+    def __next_BFS_step(self):
+        if self.ds and Search.found == None:
+            s = self.ds.pop(0)
+            print(s)
+            self.maze.tile_color[s.i][s.j][s.k] = 2
+            if self.prev_node != None:
+                self.maze.tile_color[self.prev_node.i][self.prev_node.j][self.prev_node.k] = 3
+            self.prev_node = s
+            children = s.get_children_nodes()
+            if not children:
+                return 0
+            for child in children:
+                if self.visited[child.i][child.j][child.k] == False:
+                    self.ds.append(child)
+                    self.maze.tile_color[child.i][child.j][child.k] = 1
+                    self.visited[child.i][child.j][child.k] = True
+        elif Search.found != None:
+            self.maze.solved = True
+            self.maze.print("BFS cost is " + str(Search.found.get_path_cost()))
+        else:
+            print("No Solution")
+
+    def set_mode(self, mode):
+        self.mode = mode
+        self.__reset()
+
+    def next_step(self):
+        if self.mode == 2:
+            self.__next_BFS_step()
+
 
     def get_path(self):     #method to return the path of the search
         Search.found.get_path_cost()
         return Search.found.get_path()
+
+   
+
 
     def BFS(self):  #breadth first search: Traverses the search saves the path and prints the total cost
         self.__reset()
